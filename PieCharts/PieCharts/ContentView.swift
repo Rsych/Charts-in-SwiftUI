029b5dac862835f6451eb4be272cb8774848c3cd
Ryan <tamielryan@gmail.com>
2022-01-16 22:32:33 +0200

    PieSegment

--- PieCharts/PieCharts/ContentView.swift
+++ PieCharts/PieCharts/ContentView.swift
@@ -7,6 +7,42 @@
 
 import SwiftUI
 
+struct DataPoint: Identifiable {
+    let id: Int
+    let value: Double
+    let color: Color
+    
+    init(value: Double, color: Color) {
+        self.id = Int.random(in: 1..<Int.max)
+        self.value = value
+        self.color = color
+    }
+    
+    init(id: Int, value: Double, color: Color) {
+        self.id = id
+        self.value = value
+        self.color = color
+    }
+}
+
+struct PieSegment: Shape, Identifiable {
+    func path(in rect: CGRect) -> Path {
+        let radius = min(rect.width, rect.height) / 2
+        let center = CGPoint(x: rect.width / 2, y: rect.height / 2)
+        
+        var path = Path()
+        path.move(to: center)
+        path.addRelativeArc(center: center, radius: radius, startAngle: Angle(radians: startAngle), delta: Angle(radians: amount))
+        
+        return path
+    }
+    
+    let data: DataPoint
+    var id: Int { data.id }
+    var startAngle: Double
+    var amount: Double
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
