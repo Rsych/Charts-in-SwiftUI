048ab67a8f40e936fc1857d2dbe13addd9a0e67a
Ryan <tamielryan@gmail.com>
2022-01-16 14:53:02 +0200

    Pie started

---
+++ PieCharts/PieCharts/PieChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartsApp.swift
+//  PieCharts
+//
+//  Created by Ryan J. W. Kim on 2022/01/16.
+//
+
+import SwiftUI
+
+@main
+struct PieChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
