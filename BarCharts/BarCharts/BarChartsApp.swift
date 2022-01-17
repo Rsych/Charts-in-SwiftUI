a5623aa02d0a3a386f35b41d5599519b0fb087fd
Ryan <tamielryan@gmail.com>
2022-01-17 16:13:31 +0200

    BarCharts

---
+++ BarCharts/BarCharts/BarChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartsApp.swift
+//  BarCharts
+//
+//  Created by Ryan J. W. Kim on 2022/01/17.
+//
+
+import SwiftUI
+
+@main
+struct BarChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarCharts/BarCharts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarCharts
+//
+//  Created by Ryan J. W. Kim on 2022/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
