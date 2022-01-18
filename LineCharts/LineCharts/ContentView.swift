99e3ed358213789a4aa334e416587ee12375deef
Ryan <tamielryan@gmail.com>
2022-01-18 17:56:15 +0200

    sample data generator

--- LineCharts/LineCharts/ContentView.swift
+++ LineCharts/LineCharts/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var data = makeDataPoints()
+
     var body: some View {
         Text("Hello, world!")
             .padding()
+    }
+    
+    static func makeDataPoints() -> [DataPoint] {
+        var isGoingUp = true
+        var currentValue = 50.0
+
+        return (1...50).map { _ in
+            if isGoingUp {
+                currentValue += Double.random(in: 1...10)
+            } else {
+                currentValue += -Double.random(in: 1...10)
+            }
+
+            if isGoingUp {
+                if Int.random(in: 0..<10) == 0 {
+                    isGoingUp.toggle()
+                }
+            } else {
+                if Int.random(in: 0..<7) == 0 {
+                    isGoingUp.toggle()
+                }
+            }
+
+            return DataPoint(value: abs(currentValue))
+        }
     }
 }
 
@@ -18,4 +45,8 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct DataPoint {
+    let value: Double
 }
