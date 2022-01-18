ba877fb3a5cdfd4d7064157ecf13687a4d5342df
Ryan <tamielryan@gmail.com>
2022-01-18 17:50:34 +0200

    line error

---
+++ LineCharts/LineCharts/LineChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineChartsApp.swift
+//  LineCharts
+//
+//  Created by Ryan J. W. Kim on 2022/01/17.
+//
+
+import SwiftUI
+
+@main
+struct LineChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
